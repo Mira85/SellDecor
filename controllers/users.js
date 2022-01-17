bb79fc296ae36cce6fc6687973ba3878296ca44c
Mira Makadia <patelmirak@gmail.com>
2022-01-17 14:04:54 -0500

    WIP: adds favorite route

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,9 @@
 // Dependencies
 const express = require("express");
+const mongoose = require("mongoose");
 const User = require("../models/user");
 const Item = require("../models/item");
+const user = require("../models/user");
 //Route object
 const usersRouter = express.Router();
 
@@ -14,6 +16,8 @@ usersRouter.get("/dashboard/:id", async (req, res) => {
     }
 });
 
+
+
 //create
 usersRouter.post("/", async (req, res) => {
     try {
@@ -23,6 +27,43 @@ usersRouter.post("/", async (req, res) => {
     }
 });
 
+//add favorite item to user's object
+usersRouter.post("/add_favorite", async (req, res) => {
+    try {
+        const user = await User.findById(req.query.user_id);
+        const item = req.query.item_id;
+        user.favorites.push(item);
+
+        await user.save();
+        res.status(200);
+        res.end();
+    } catch (error) {
+        res.status(400).json(error);
+    }
+
+
+});
+
+//delete favorite
+
+usersRouter.delete("/delete_favorite", async (req, res) => {
+    try {
+        const user = await User.findById(req.query.user_id);
+        const itemId = mongoose.Types.ObjectId(req.query.item_id) ;
+         //console.log(itemId)
+       user.favorites = user.favorites.filter(objId => 
+        {
+            console.log(objId, itemId);
+            objId !== itemId;
+        });
+       console.log(user)
+        await user.save();
+        res.status(200);
+        res.end();
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
 
 //export route object
 module.exports = usersRouter;
