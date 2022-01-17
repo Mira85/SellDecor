668d7bbb463aa4ce4f509d36e636c9db733c2a97
Mira Makadia <patelmirak@gmail.com>
2022-01-17 11:16:16 -0500

    WIP:adds dashboard route

--- controllers/users.js
+++ controllers/users.js
@@ -1,13 +1,14 @@
 // Dependencies
 const express = require("express");
 const User = require("../models/user");
+const Item = require("../models/item");
 //Route object
 const usersRouter = express.Router();
 
-
-usersRouter.get("/:id", async (req, res) => {
+// dashboard route
+usersRouter.get("/dashboard/:id", async (req, res) => {
     try {
-        res.json(await User.findById(req.params.id));
+        res.json(await User.findById(req.params.id).populate('favorites'));
     } catch (error) {
         res.status(400).json(error);
     }
