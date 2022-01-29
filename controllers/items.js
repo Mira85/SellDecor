9feced7fdabfe045920e891c1f8bdbfac7e1b69a
Mira Makadia <patelmirak@gmail.com>
2022-01-29 10:11:07 -0500

    formats code

--- controllers/items.js
+++ controllers/items.js
@@ -7,105 +7,75 @@ const User = require("../models/user");
 const itemsRouter = express.Router();
 
 async function isAuthenticated(req, res, next) {
-  //console.log(req)
-    try{
+    //console.log(req)
+    try {
         const token = req.get("Authorization");
-    if(!token) throw new Error("you must be logged in first")
+        if (!token) throw new Error("you must be logged in first")
 
-    const user = await admin.auth().verifyIdToken(token.replace("Bearer ", ""));
-    if(!user) throw new Error ("something went wrong");
+        const user = await admin.auth().verifyIdToken(token.replace("Bearer ", ""));
+        if (!user) throw new Error("something went wrong");
 
-    req.user = user;
+        req.user = user;
 
-    next();
+        next();
 
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(400).json({ message: error.message });
 
     }
-    
+
 };
 
 async function getUser(uId) {
-    const user = await User.findOne({uId : uId});
+    const user = await User.findOne({ uId: uId });
     return user;
-} 
+}
 
-//Routes
-//Index
-/* itemsRouter.get("/", isAuthenticated, async (req, res) => {
-    try {
-        const user = await getUser(req.user.uid);
-        res.json(user.itemsToSell);
-
-            // res.json(await Item.find({uId: req.user.uid}))
-    } catch (error) {
-        res.status(400).json(error);
-    }
-}); */
 
 
 itemsRouter.get("/", async (req, res) => {
     try {
         res.json(await Item.find({}));
-     
 
-            // res.json(await Item.find({uId: req.user.uid}))
     } catch (error) {
         res.status(400).json(error);
     }
 });
 
 
-/* //get category items
-
-itemsRouter.get("/:category", async (req, res) => {
-    try {
-        if (req.params.category){
-            res.json(await Item.find({"category":req.params.category}))
-        } else {
-            res.json(await Item.find({}));
-
-        }
-      //  res.json(await Item.find({"category":categoryValue}));
-      
-    } catch (error) {
-        res.status(400).json(error);
-    }
-}); */
 
 //delete
 itemsRouter.delete("/:deleteItemId", isAuthenticated, async (req, res) => {
     try {
         await Item.findByIdAndDelete(req.params.deleteItemId)
-        
-       
-       const user = await getUser(req.user.uid)
+
+
+        const user = await getUser(req.user.uid)
         console.log(user)
-      
+
 
         user.itemsToSell = user.itemsToSell.filter(objId => objId != req.params.deleteItemId);
         await user.save();
 
-      res.status(200);
-      res.end();
+        res.status(200);
+        res.end();
     } catch (error) {
-      //send error
-      res.status(400).json(error)
+        //send error
+        res.status(400).json(error)
     }
-  });
-  
+});
+
 
 
 //create
 itemsRouter.post("/", isAuthenticated, async (req, res) => {
-   
+
     try {
         req.body.uId = req.user.uid;
         console.log(req.body);
         const newItem = await Item.create(req.body);
-       
-       
+
+
         const user = await getUser(req.user.uid)
         console.log(user)
         user.itemsToSell.push(newItem._id);
--- controllers/users.js
+++ controllers/users.js
@@ -13,52 +13,30 @@ const usersRouter = express.Router();
 const serviceAccount = require("../service-account-credentials.json");
 
 async function isAuthenticated(req, res, next) {
-  //console.log(req)
-    try{
+    //console.log(req)
+    try {
         const token = req.get("Authorization");
-    if(!token) throw new Error("you must be logged in first")
+        if (!token) throw new Error("you must be logged in first")
 
-    const user = await admin.auth().verifyIdToken(token.replace("Bearer ", ""));
-    if(!user) throw new Error ("something went wrong");
+        const user = await admin.auth().verifyIdToken(token.replace("Bearer ", ""));
+        if (!user) throw new Error("something went wrong");
 
-    req.user = user;
+        req.user = user;
 
-    next();
+        next();
 
     } catch (error) {
-        res.status(400).json({message: error.message});
+        res.status(400).json({ message: error.message });
 
     }
-    
+
 };
 
 
-
-/* async function isAuthenticated(req, res, next) {
-    console.log("req.auth", req.headers)
-    try{
-        const token = req.get("Authorization");
-    if(!token) throw new Error("you must be logged in first")
-
-    const user = await admin.auth().verifyIdToken(token.replace("Bearer ", ""));
-    if(!user) throw new Error ("something went wrong");
-
-    req.user = user;
-
-    next();
-
-    } catch (error) {
-        res.status(400).json({message: error.message});
-
-    }
-    
-}
- */
-
 async function getUser(uId) {
-    const user = await User.findOne({uId : uId}).populate('favorites').populate('itemsToSell');
+    const user = await User.findOne({ uId: uId }).populate('favorites').populate('itemsToSell');
     return user;
-} 
+}
 //get user
 
 usersRouter.get("/", isAuthenticated, async (req, res) => {
@@ -70,23 +48,14 @@ usersRouter.get("/", isAuthenticated, async (req, res) => {
     } catch (error) {
         res.status(400).json(error);
     }
-}); 
+});
 
-/* usersRouter.get("/:uid", async (req, res) => {
-
-console.log(req.user);
-    try {
-        res.json(await User.findOne({uId : req.params.uid}));
-    } catch (error) {
-        res.status(400).json(error);
-    }
-}); */
 
 // dashboard route
 usersRouter.get("/userinfo", isAuthenticated, async (req, res) => {
     try {
-         const user = await getUser(req.user.uid);
-         res.json(user);
+        const user = await getUser(req.user.uid);
+        res.json(user);
     } catch (error) {
         res.status(400).json(error);
     }
@@ -97,42 +66,42 @@ usersRouter.get("/userinfo", isAuthenticated, async (req, res) => {
 //create
 usersRouter.post("/", isAuthenticated, async (req, res) => {
     console.log(req.get("Authorization"))
-     req.body.uId = req.body.uid;
-    
-     try {
+    req.body.uId = req.body.uid;
+
+    try {
         res.json(await User.create(req.body));
-     } catch (error) {
+    } catch (error) {
         res.status(400).json(error);
-      }
+    }
 
 
-   
+
 });
 
 //delete favorite
 
 usersRouter.delete("/delete_favorite", isAuthenticated, async (req, res) => {
-       
-     try {
+
+    try {
         const user = await getUser(req.user.uid);
         const itemId = req.query.item_id;
 
-       user.favorites = user.favorites.filter(object => object._id != itemId);
-        
+        user.favorites = user.favorites.filter(object => object._id != itemId);
+
         await user.save();
         res.status(200);
         res.end();
     } catch (error) {
         res.status(400).json(error);
-    } 
+    }
 });
 
 
 //delete user
 
 usersRouter.delete("/:id", isAuthenticated, async (req, res) => {
-    try{
-    res.json(await User.findByIdAndDelete(req.params.id));
+    try {
+        res.json(await User.findByIdAndDelete(req.params.id));
     } catch (error) {
         res.status(400).json(error);
     }
@@ -141,8 +110,8 @@ usersRouter.delete("/:id", isAuthenticated, async (req, res) => {
 //update user
 
 usersRouter.put("/:id", isAuthenticated, async (req, res) => {
-    try{
-        res.json(await User.findByIdAndUpdate(req.params.id, req.body, {new:true}));
+    try {
+        res.json(await User.findByIdAndUpdate(req.params.id, req.body, { new: true }));
 
     } catch (error) {
         res.status(400).json(error);
@@ -153,9 +122,9 @@ usersRouter.put("/:id", isAuthenticated, async (req, res) => {
 usersRouter.post("/add_favorite", isAuthenticated, async (req, res) => {
     try {
         //const user = await User.findById(req.query.user_id);
-    
+
         const item = req.query.item_id;
-         const user = await getUser(req.user.uid);
+        const user = await getUser(req.user.uid);
         user.favorites.push(item);
 
         await user.save();
